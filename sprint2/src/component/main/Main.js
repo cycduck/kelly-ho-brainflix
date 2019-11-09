e01427fdbe8e00537ff2be8d165a7d8e78cef387
Kelly K H <kh.waterloo@gmail.com>
2019-11-09 00:11:54 -0500

    seems that I can fetch from master, redoing from square one on this branch

--- sprint2/src/component/main/Main.js
+++ sprint2/src/component/main/Main.js
@@ -20,45 +20,14 @@ const vidURL = `${url}/videos${key}`;
 
 export default class Main extends React.Component {
   // JS class world, can't use declarations
-  constructor(props) {
-    // older version??? REALLY??????? What does it do??? Benefits?
-    console.log(props)
-    super(props);
-    this.state = {
-      mainVidInfo: mainVideo,
-      sideVidInfo: sideVideo
-    }
+  state = {
+    mainVidInfo: mainVideo,
+    sideVidInfo: sideVideo
   }
   
-  axiosRun = (url) => {
-    Axios.all([
-        // https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/
-        Axios.get(url),
-        Axios.get(vidURL)
-      ])
-      .then(Axios.spread((main, side) => {
-        console.log(main, side)
-        this.setState({
-          mainVidInfo: main.data,
-          sideVidInfo: side.data
-        })
-      }))
-  }
-
-  componentDidMount() {
-    // prefer function so that it loads AFTER component renders
-    this.axiosRun('https://project-2-api.herokuapp.com/videos/1af0jruup5gu?api_key=add3816a-9a16-42e2-8a1c-a9c9c9400638')
-  }
-
-  componentDidUpdate() {
-    // when the page updates 
-    // do something...
-    if(this.state.mainVidInfo.id !== this.props.match.params.vidID) {
-    (() => {
-      let x = `${url}/videos/${this.props.match.params.vidID}${key}`
-      this.axiosRun(x)
-    }) ()
-    }
+  sideVidFilter = () => {
+    let filtered = this.state.sideVidInfo.filter(i => i.id !== this.state.mainVidInfo.id);
+    return filtered
   }
 
   
--- sprint2/src/component/nav/Nav.js
+++ sprint2/src/component/nav/Nav.js
@@ -5,13 +5,8 @@ import avatar from '../../assets/Images/Mohan-muruge.jpg';
 import {Link} from 'react-router-dom';
 
 class Nav extends React.Component {
-    // what is the react.component?
-    // you can also use Comoonent
-    // every component must extend from the React component
-    // file:///F:/webDev/Hackeryou/Beginner/dayFour/airbnb_codealong_answer/airbnb-login-form.html
-    // mouseover
+
     render() {
-        // why is the render needed?
         return (
             <nav className="nav">
                 <img className="nav__logo" src={logoImg} alt="Brainflix logo"/>
